e19149e5803ccfc0b8b75a03d67954b51fea5884
Maxkambek <maxkambekxolbekov515@gmail.com>
2022-08-15 12:21:06 +0500

    08

--- bin/list_index08.dart
+++ bin/list_index08.dart
@@ -6,4 +6,23 @@
     Returns:
         list: return answer
 */
-void main() {}
+List func(List list1){
+  int a = 0;
+  int c = 0;
+while(a < list1.length){
+if (list1[a]==1){
+  list1[a]=true;
+    }
+    else if(c < list1.length){
+      if (list1[c]==0){
+        list1[c]=false;
+      }
+    }
+    a += 1;
+    c += 1;
+  }
+  return list1;
+}
+void main() {
+  print(func([1,0,0,0,0]));
+}
